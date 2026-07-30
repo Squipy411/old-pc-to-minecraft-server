7d1b999886d53f389c38faf96b01ed6d13172bd9
Squipy411 <80914301+Squipy411@users.noreply.github.com>
2026-07-29 23:49:03 -0700

    Prepare Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  webpack(config) {
+    config.module.rules.push({ test: /\.mdx$/, type: "asset/source" });
+    return config;
+  },
 };
 
 export default nextConfig;
